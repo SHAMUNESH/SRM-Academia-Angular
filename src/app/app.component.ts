27aba6368796839347ce799ee9596563912ca18d
Shamunesh Selvam <64699355+SHAMUNESH@users.noreply.github.com>
2022-02-15 18:53:22 +0530

    local-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+// import { faCoffee} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first-angular-app';
+
+export class AppComponent implements OnInit {
+  title = 'shamunesh';
+  ImagePath: string;
+  // faCoffee =faCoffee;
+  
+  constructor() {
+    
+    this.ImagePath = '/assets/images/landerpage.jpg'
+  }
+  
+  ngOnInit() {
+  }
+  
 }
